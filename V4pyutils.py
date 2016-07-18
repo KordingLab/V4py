ead8c415798e57211c4f07471e406a04e411d221
KordingLab <hugoguh@gmail.com>
2016-07-17 20:43:25 -0500

    added more visualizations

--- V4pyutils.py
+++ V4pyutils.py
@@ -12,12 +12,20 @@ import cv2
 # stats
 #import pycircstat as pyc
 from scipy import stats
+import statsmodels.api as sm
+
 # spykes
 from neuropop import NeuroPop
 from neurovis import NeuroVis
 
 import xgboost as xgb
 from sklearn.cross_validation import KFold
+from sklearn.cross_validation import LabelKFold
+
+import matplotlib.pyplot as plt
+
+
+
 
 #---------------------------------------
 # Helpers for feature extraction
@@ -148,10 +156,10 @@ def XGB_poisson(Xr, Yr, Xt):
     'seed': 2925,
     'silent': 1,
     'missing': '-999.0'}
-    param['nthread'] = -1
+    param['nthread'] = 12
 
-    dtrain = xgb.DMatrix( Xr, label=Yr)
-    dtest = xgb.DMatrix( Xt)
+    dtrain = xgb.DMatrix(Xr, label=Yr)
+    dtest = xgb.DMatrix(Xt)
 
     num_round = 200
     bst = xgb.train( param, dtrain, num_round )
@@ -160,13 +168,18 @@ def XGB_poisson(Xr, Yr, Xt):
     return Yt
 
 #---------------------------------------
-def fit_cv(X, Y, algorithm = 'XGBoost',n_cv=10, verbose=1):
+def fit_cv(X, Y, algorithm = 'XGBoost',n_cv=10,
+           verbose=1, label=[]):
 
     if np.ndim(X)==1:
         X = np.transpose(np.atleast_2d(X))
 
     #skf = StratifiedKFold(Y, n_cv, shuffle=True, random_state=1)
-    skf  = KFold(n=np.size(Y), n_folds=n_cv, shuffle=False,random_state=None)
+    if len(label)>0:
+        skf  = LabelKFold(np.squeeze(label), n_folds=n_cv)
+    else:
+        skf  = KFold(n=np.size(Y), n_folds=n_cv, shuffle=True, random_state=42)
+
     #skf  = LabelKFold(X['trial'].values, n_folds=n_cv)
 
     i=1
@@ -195,3 +208,196 @@ def fit_cv(X, Y, algorithm = 'XGBoost',n_cv=10, verbose=1):
         print("pR2_cv: %0.6f (+/- %0.6f)" % (np.mean(pR2_cv), np.std(pR2_cv)/np.sqrt(n_cv)))
 
     return Y_hat, pR2_cv
+
+# -----------------------------------------------------------------
+def plot_predicted_vs_counts(models_for_plot, Y = None, models = None,
+                             title = '',
+                             colors=['#F5A21E', '#134B64', '#EF3E34', '#02A68E', '#FF07CD'], ylim=None,
+                             simul=False):
+
+    plt.title(title)
+
+    unique_counts = np.unique(Y)
+
+    for i, model in enumerate(models_for_plot):
+        if simul:
+            Ycounts = np.random.poisson(models[model]['Yt_hat'])
+        else:
+            Ycounts = Y
+
+        if len(models_for_plot)==1:
+
+
+            plt.plot(Ycounts + 0.2*np.random.normal(size=np.size(Ycounts)),
+                 models[model]['Yt_hat'],
+                 'k.', alpha=0.05, markersize=20)
+
+        meanYhat = list()
+        semYhat = list()
+
+        for npks in unique_counts:
+            loc = np.where(Ycounts==npks)[0]
+            meanYhat.append(np.mean(models[model]['Yt_hat'][loc]))
+            semYhat.append(np.std(models[model]['Yt_hat'])/np.sqrt(len(loc)))
+
+        plt.plot(np.unique(Y), meanYhat, '.', color=colors[i],  ms=15, alpha=0.9)
+        plt.errorbar(np.unique(Y), meanYhat, fmt='none', yerr=np.array(semYhat), alpha=0.5, ecolor=colors[i])
+
+    plt.ylabel('predicted (Y_hat)')
+    if simul:
+        plt.xlabel('simulated spike counts (Y)')
+    else:
+        plt.xlabel('spike counts (Y)')
+
+    if ylim:
+        plt.ylim(ylim)
+    else:
+        plt.axis('equal')
+
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.tick_params(axis='y', right='off')
+    plt.tick_params(axis='x', top='off')
+    if len(models_for_plot)>1:
+        plt.legend(models_for_plot, frameon=False, loc=0)
+    else:
+        plt.legend([''] + models_for_plot, frameon=False, loc=0)
+
+    #plt.axis('equal')
+
+# -----------------------------------------------------------------
+def plot_model_vs_model(models_for_plot, models=None, title=''):
+    max_val = np.max(models[models_for_plot[1]]['Yt_hat'])
+    min_val = np.min(models[models_for_plot[1]]['Yt_hat'])
+    plt.plot([min_val,max_val],[min_val,max_val], '-r', lw=0.6)
+    plt.plot(models[models_for_plot[0]]['Yt_hat'], models[models_for_plot[1]]['Yt_hat'], 'k.', alpha=0.1, ms=10)
+
+    plt.xlabel('model ' + models_for_plot[0])
+    plt.ylabel('model ' + models_for_plot[1])
+
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.tick_params(axis='y', right='off')
+    plt.tick_params(axis='x', top='off')
+    plt.title(title)
+    plt.axis('equal')
+
+# -----------------------------------------------------------------
+def plot_model_vs_time(model_for_plot, Y=None, models=None,
+                       x_variable=None,
+                       title=False, lowess_frac=0.1, simul=False,
+                       color='r'):
+
+    lowess = sm.nonparametric.lowess
+
+    if simul:
+        y_counts = np.random.poisson(models[model_for_plot]['Yt_hat'])
+    else:
+        y_counts = Y
+
+    w = lowess(y_counts, x_variable, frac=lowess_frac)
+
+
+    plt.plot(x_variable, y_counts+0.5*np.random.rand(np.size(y_counts)), 'k.', alpha=0.1, ms=10)
+    plt.plot(x_variable, models[model_for_plot]['Yt_hat'], '.', c=color, alpha=0.5, lw=0.3)
+    plt.plot(w[:,0], w[:,1], 'k', lw=3)
+
+    if title:
+        plt.title(title)
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.tick_params(axis='y', right='off')
+    plt.tick_params(axis='x', top='off')
+    plt.xlabel('time [s]')
+    plt.ylabel('spk counts; Yt_hat')
+    if simul:
+        plt.legend(['simul counts (data)','model %s' % model_for_plot,'smoothed simul data'],
+                   frameon=False, loc=0)
+    else:
+        plt.legend(['counts (data)','model %s' % model_for_plot,'smoothed data'], frameon=False, loc=0)
+
+
+# -----------------------------------------------------------------
+def plot_lowess_vs_lowess(model_for_plot, Y=None, models=None,
+                       x_variable='',
+                       title=False, lowess_frac=0.1,
+                       color='r'):
+
+    lowess = sm.nonparametric.lowess
+
+
+    y_counts_simul = np.random.poisson(models[model_for_plot]['Yt_hat'])
+    y_counts = Y
+
+    w = lowess(y_counts, x_variable, frac=lowess_frac)
+    w_simul = lowess(y_counts_simul, x_variable, frac=lowess_frac)
+
+
+    plt.plot(x_variable, y_counts+0.5*np.random.rand(np.size(y_counts)), 'k.', alpha=0.1, ms=10)
+    plt.plot(x_variable, models[model_for_plot]['Yt_hat'], '.', c=color, alpha=0.5, lw=0.3)
+    plt.plot(w[:,0], w[:,1], 'k', lw=3)
+    plt.plot(w_simul[:,0], w_simul[:,1], 'm', lw=3)
+
+    if title:
+        plt.title(title)
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.tick_params(axis='y', right='off')
+    plt.tick_params(axis='x', top='off')
+    plt.xlabel('time [s]')
+    plt.ylabel('spk counts; Yt_hat')
+
+    plt.legend(['counts (data)','model %s' % model_for_plot,'smoothed data','smoothed simul data'], frameon=False, loc=0)
+
+# -----------------------------------------------------------------
+def plot_model_comparison(models_for_plot, models=[], color='r', title=None):
+
+    plt.plot([-1, len(models_for_plot)],[0,0],'--k', alpha=0.4)
+
+    mean_pR2 = list()
+    sem_pR2 = list()
+
+    for model in models_for_plot:
+        PR2_art = models[model]['PR2']
+        mean_pR2.append(np.mean(PR2_art))
+        sem_pR2.append(np.std(PR2_art)/np.sqrt(np.size(PR2_art)))
+
+    plt.bar(np.arange(np.size(mean_pR2)), mean_pR2, 0.8, align='center',
+            ecolor='k', alpha=0.3, color=color, ec='w', yerr=np.array(sem_pR2),
+            tick_label=models_for_plot)
+    plt.plot(np.arange(np.size(mean_pR2)), mean_pR2, 'k.', markersize=15)
+
+    plt.ylabel('pseudo-R2')
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.tick_params(axis='y', right='off')
+    plt.tick_params(axis='x', top='off')
+    if title:
+        plt.title(title)
+
+# -----------------------------------------------------------------
+def plot_tuning_curve(models_for_plot, hues=[], Y=[], models=[], title='',
+                      colors=['#F5A21E', '#134B64', '#EF3E34', '#02A68E', '#FF07CD']):
+
+    plt.plot(hues, Y + 0.01*np.random.normal(size=np.size(hues)),
+             'k.', alpha=0.1, markersize=20)
+
+    for i, model in enumerate(models_for_plot):
+        plt.plot(hues, models[model]['Yt_hat'], '.', color=colors[i], alpha = 0.5)
+
+    plt.xlabel('hue angle [rad]')
+    plt.ylabel('spike counts')
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.tick_params(axis='y', right='off')
+    plt.tick_params(axis='x', top='off')
+
+    plt.legend(['counts (Y)'] + models_for_plot, frameon=False)
+    if title:
+        plt.title(title)
